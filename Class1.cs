0ccb16f3970c40d2a16132861bfc68db031d2562
Jessie Houghton <houghj@umich.edu>
2022-10-19 13:32:47 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathLibrary
+{
+    public class Class1
+    {
+        public float Substract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float Devide(float a, float b)
+        {
+            return a / b;
+        }
+
+        public float Power(float a)
+        {
+            return a * a;
+        }
+    }
+}
